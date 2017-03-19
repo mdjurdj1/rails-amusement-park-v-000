e626691d2af9f9881522335d4e063d12adcfcce6
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-19 18:07:16 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,7 +2,11 @@ class RidesController < ApplicationController
 
   def new
     @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
-    redirect_to user_path(current_user), :alert => "Thanks for riding!"
+    if !@ride.take_ride.is_a?(String) 
+      redirect_to user_path(@ride.user), :alert => "Thanks for riding the #{@ride.attraction.name}!"
+    else
+      redirect_to user_path(@ride.user), :alert => @ride.take_ride
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new'
   get '/logout' => 'sessions#destroy'
   post '/signin' => 'sessions#create'
+  post '/rides/new' => 'rides#new'
   resources :users
   resources :attractions
 end
